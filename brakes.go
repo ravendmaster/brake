2a9066a4ff1cb26c1f61386fefd9933e561b2c3c
Andrey <ravendmaster@gmail.com>
2021-04-04 21:31:22 +0300

    tests

--- brakes.go
+++ brakes.go
@@ -8,38 +8,23 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
 func main() {
 
-	state := Open()
+	ctx := Open()
+	defer Close(ctx)
+
+	go doFillTest(ctx, "test")
+	go doFillTest(ctx, "alpha")
+	go doFillTest(ctx, "betta")
+	go doFillTest(ctx, "gamma")
 
 	/*
-		message := ""
-		for i := 0; i < 512; i++ {
-			message += "1"
-		}
-		start := time.Now().UnixNano()
-		for i := 1; i <= 32000; i++ {
-			Put(state, "test", strconv.Itoa(i)+" "+message)
-		}
-		println("put time", (time.Now().UnixNano()-start)/1000/1000)
 
-		start = time.Now().UnixNano()
-		id := 1
-		for i := 0; i < 32000; i++ {
-			message := Get(state, "test", id)
-			if message.Id == -1 {
-				println("no more")
-				break
-			}
-			id = message.Id + 1
-
-			//println(message.id)
-
-		}
-		println("get time", (time.Now().UnixNano()-start)/1000/1000)
 
 		println("first:", Info(state, "test").FirstId)
 		println("last:", Info(state, "test").LastId)
@@ -73,28 +58,81 @@ func main() {
 
 		switch r.Method {
 		case "POST":
-			processPOSTMethod(state, w, r)
+			processPOSTMethod(ctx, w, r)
 		case "GET":
-			processGETMethod(state, w, r)
+			processGETMethod(ctx, w, r)
 		}
 	})
 
 	http.ListenAndServe(":80", nil)
 
-	Close(state)
-
 }
+
+func doFillTest(ctx *brakesContext, queue string) {
+
+	//Put(ctx, "test", "hi")
+	//Get(ctx, "test", 9872938293874)
+
+	message := ""
+	for i := 0; i < 512; i++ {
+		message += "1"
+	}
+
+	queueInfo := Info(ctx, queue)
+
+	id := queueInfo.LastId + 1
+	if queueInfo.LastId == -1 {
+		id = 1
+	}
+
+	idForRead := id
+
+	start := time.Now().UnixNano()
+	for i := 1; i <= 5000000; i++ {
+		Put(ctx, queue, strconv.Itoa(id)+"             "+message)
+		id++
+	}
+	println("put time", (time.Now().UnixNano()-start)/1000/1000)
+
+	start = time.Now().UnixNano()
+
+	for i := 0; i < 50000; i++ {
+		message := Get(ctx, queue, idForRead)
+		if message.Id == -1 {
+			println("no more", idForRead)
+			break
+		}
+
+		idForRead = message.Id + 1
+
+		res, _ := strconv.Atoi(strings.Split(message.Message, " ")[0])
+		if res != message.Id {
+			//	panic("error!")
+		}
+
+		//println(message.id)
+
+	}
+	println("get time", (time.Now().UnixNano()-start)/1000/1000)
+
+	queueInfo = Info(ctx, queue)
+	println("messages from", queueInfo.FirstId, "to", queueInfo.LastId)
+}
+
+const dataFileExtension = ".data"
+const indexFileExtension = ".idx"
+const putStateFileExtension = ".pst"
 
 type brakesInfo struct {
 	LastId uint64
 }
 
 type incommingHttpMessage struct {
-	Queue string
-	Msg   string
+	Queue string `json:"queue"`
+	Msg   string `json:"msg"`
 }
 
-func processPOSTMethod(state *State, w http.ResponseWriter, r *http.Request) {
+func processPOSTMethod(ctx *brakesContext, w http.ResponseWriter, r *http.Request) {
 
 	body, _ := io.ReadAll(r.Body)
 
@@ -103,7 +141,7 @@ func processPOSTMethod(state *State, w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal([]byte(body), &incommingMessage)
 
 	for _, item := range incommingMessage {
-		Put(state, item.Queue, item.Msg)
+		go Put(ctx, item.Queue, item.Msg)
 	}
 
 	res := brakesInfo{LastId: 1}
@@ -113,22 +151,22 @@ func processPOSTMethod(state *State, w http.ResponseWriter, r *http.Request) {
 }
 
 type responseHttpMessage struct {
-	Id  int64
-	Msg string
+	Id  int64  `json:"id"`
+	Msg string `json:"msg"`
 }
 
-func processGETMethod(state *State, w http.ResponseWriter, r *http.Request) {
+func processGETMethod(ctx *brakesContext, w http.ResponseWriter, r *http.Request) {
 
 	values := r.URL.Query()
 
-	queue_name := values["queue"][0]
+	queueName := values["queue"][0]
 	id, _ := strconv.ParseInt(values["id"][0], 10, 64)
 	limit, _ := strconv.Atoi(values["limit"][0])
 
 	var res []responseHttpMessage
 
 	for {
-		msg := Get(state, queue_name, int(id))
+		msg := Get(ctx, queueName, int(id))
 		if msg.Id == -1 {
 			break
 		}
@@ -145,17 +183,21 @@ func processGETMethod(state *State, w http.ResponseWriter, r *http.Request) {
 
 	}
 
-	json_data, _ := json.Marshal(res)
-	fmt.Fprint(w, string(json_data))
+	jsonData, _ := json.Marshal(res)
+	fmt.Fprint(w, string(jsonData))
 }
 
-func Open() *State {
-	state := new(State)
-	state.files = make(map[string]*os.File)
+func Open() *brakesContext {
+	ctx := new(brakesContext)
+
+	ctx.rootDataDir = `d:\temp\`
+
+	ctx.files = make(map[string]*os.File)
+	ctx.queuesContexts = make(map[string]*queueContext)
 
 	go func() {
 		for {
-			for _, f := range state.files {
+			for _, f := range ctx.files {
 				f.Sync()
 			}
 			time.Sleep(time.Duration(1) * time.Second)
@@ -163,24 +205,42 @@ func Open() *State {
 
 	}()
 
-	return state
+	return ctx
 }
 
-func Close(state *State) {
-	for _, f := range state.files {
+func Close(ctx *brakesContext) {
+	for _, f := range ctx.files {
 		f.Close()
 	}
 }
 
-type State struct {
-	files map[string]*os.File
+type partContext struct {
+	firstMessageId int
+	lastMessageId  int
 }
 
-func openIndexFileForRead(state *State, queue string, tic_tac_suffix string) (*os.File, int, int) {
+type queueContext struct {
+	initialized              bool
+	activePartNo             int
+	firstMessageIdFowNewPart int
+	startTime                int64
 
-	f_idx := getFile(state, queue+tic_tac_suffix+".idx")
+	partsContexts []*partContext
 
-	first_id := -1
+	sync.Mutex
+}
+
+type brakesContext struct {
+	rootDataDir    string
+	files          map[string]*os.File
+	queuesContexts map[string]*queueContext
+}
+
+func openIndexFile(ctx *brakesContext, queue string, storagePart string) (*os.File, int, int) {
+
+	f_idx := openFile(ctx, queue+storagePart+indexFileExtension)
+
+	firstId := -1
 	count := 0
 
 	pos, err := f_idx.Seek(0, io.SeekEnd)
@@ -188,16 +248,41 @@ func openIndexFileForRead(state *State, queue string, tic_tac_suffix string) (*o
 		println(err)
 	}
 	if pos > 0 {
-		first_id = int(readUint64At(f_idx, 0))
-		count = int(pos>>3 - 1)
+		firstId = int(readUint64At(f_idx, 0))
+		count = messageCountByIndexFileSize(int(pos))
 	}
 
-	return f_idx, first_id, count
+	return f_idx, firstId, count
 }
 
-func ticTacList() (result []string) {
+func storageParts(priorityPart int) (result []int) {
 
-	return []string{"_0", "_1", "_2", "_3", "_4", "_5", "_6", "_7"}
+	switch priorityPart {
+	case 0:
+		return []int{0, 1, 2, 3}
+	case 1:
+		return []int{1, 0, 2, 3}
+	case 2:
+		return []int{2, 0, 1, 3}
+	case 3:
+		return []int{3, 0, 1, 2}
+	}
+
+	panic("error")
+}
+
+func suffixOfPart(part int) string {
+	switch part {
+	case 0:
+		return "_0"
+	case 1:
+		return "_1"
+	case 2:
+		return "_2"
+	case 3:
+		return "_3"
+	}
+	panic("suffixOfPart() overflow")
 }
 
 type QueueInfo struct {
@@ -205,22 +290,22 @@ type QueueInfo struct {
 	LastId  int
 }
 
-func Info(state *State, queue string) (info QueueInfo) {
+func Info(ctx *brakesContext, queue string) (info QueueInfo) {
 
 	info.FirstId = -1
 	info.LastId = -1
 
-	for _, tic_tac_part := range ticTacList() {
+	for _, storagePart := range storageParts(0) {
 
-		_, first_id, count := openIndexFileForRead(state, queue, tic_tac_part)
+		_, firstId, count := openIndexFile(ctx, queue, suffixOfPart(storagePart))
 
-		if (info.FirstId == -1 || first_id < info.FirstId) && first_id != -1 {
-			info.FirstId = first_id
+		if (info.FirstId == -1 || firstId < info.FirstId) && firstId != -1 {
+			info.FirstId = firstId
 		}
 
-		last_id := first_id + count - 1
-		if last_id > info.LastId {
-			info.LastId = last_id
+		lastId := firstId + count - 1
+		if lastId > info.LastId {
+			info.LastId = lastId
 		}
 	}
 
@@ -232,66 +317,98 @@ type Message struct {
 	Message string
 }
 
-func Get(state *State, queue string, message_id int) (msg Message) {
+func getQueueContext(ctx *brakesContext, queue string) *queueContext {
+	if ctx.queuesContexts[queue] == nil {
+		ctx.queuesContexts[queue] = &queueContext{partsContexts: make([]*partContext, len(storageParts(0)))}
+		for _, storagePart := range storageParts(0) {
+			ctx.queuesContexts[queue].partsContexts[storagePart] = &partContext{}
+		}
+	}
+	return ctx.queuesContexts[queue]
+}
 
-	if message_id == 0 {
-		message_id = Info(state, queue).LastId
+func predictPart(ctx *brakesContext, queue string, id int) int {
+	result := 0
+	for _, storagePart := range storageParts(0) {
+
+		partCtx := getQueueContext(ctx, queue).partsContexts[storagePart]
+
+		if id >= partCtx.firstMessageId && id <= partCtx.lastMessageId {
+			result = storagePart
+			break
+		}
+	}
+	return result
+}
+
+func updatePredictData(ctx *brakesContext, queue string, storagePart int, firstId int, count int) {
+
+	partCtx := getQueueContext(ctx, queue).partsContexts[storagePart]
+
+	if (partCtx.firstMessageId != firstId) || (partCtx.lastMessageId != firstId+count-1) {
+		partCtx.firstMessageId = firstId
+		partCtx.lastMessageId = firstId + count - 1
+	}
+}
+
+func Get(ctx *brakesContext, queue string, id int) (msg Message) {
+
+	if id == 0 {
+		id = Info(ctx, queue).LastId
 	}
 
 	msg.Id = -1
 	var beside_f_idx *os.File
-	beside_tic_tac_preffix := ""
-	beside_id_delta := 9223372036854775807 //max int
-	var beside_idx_start_id int
+	beside_storagePart := ""
+	beside_idDelta := 9223372036854775807 //max int
+	var beside_idxStartId int
 
-	for _, tic_tac_preffix := range ticTacList() {
+	priorityPart := predictPart(ctx, queue, id)
 
-		f_idx, first_id, count := openIndexFileForRead(state, queue, tic_tac_preffix)
+	for _, storagePart := range storageParts(priorityPart) {
 
-		if f_idx != nil && count > 0 {
+		f_idx, firstId, count := openIndexFile(ctx, queue, suffixOfPart(storagePart))
+		if count > 0 {
 
-			if message_id >= first_id && message_id < (first_id+count) {
+			updatePredictData(ctx, queue, storagePart, firstId, count)
 
-				data_file_pos := readUint64At(f_idx, int64(message_id-first_id+1)<<3)
-				f_data := getFile(state, queue+tic_tac_preffix+".data")
+			if id >= firstId && id < (firstId+count) {
 
-				message_size := readUint32At(f_data, int64(data_file_pos))
-				message_raw := make([]byte, message_size)
-				f_data.ReadAt(message_raw, int64(data_file_pos+4))
-				msg.Id = int(message_id)
-				msg.Message = string(message_raw)
+				dataFilePos := readUint64At(f_idx, int64(id-firstId+1)<<3)
+				f_data := openFile(ctx, queue+suffixOfPart(storagePart)+dataFileExtension)
+				msg.Id = int(id)
+				msg.Message = readMessage(f_data, dataFilePos)
 				return msg
 			}
-
-			if message_id < first_id {
-				temp_id_delta := int(first_id - message_id)
-
-				if temp_id_delta > 0 && (temp_id_delta < beside_id_delta) {
-					beside_id_delta = temp_id_delta
+			if id < firstId {
+				tempIdDelta := int(firstId - id)
+				if tempIdDelta > 0 && (tempIdDelta < beside_idDelta) {
+					beside_idDelta = tempIdDelta
 					beside_f_idx = f_idx
-					beside_tic_tac_preffix = tic_tac_preffix
-					beside_idx_start_id = first_id
+					beside_storagePart = suffixOfPart(storagePart)
+					beside_idxStartId = firstId
 				}
-
 			}
-
 		}
 
 	}
 
 	if beside_f_idx != nil {
-
-		f_data := getFile(state, queue+beside_tic_tac_preffix+".data")
-
-		data_file_pos := 0
-		message_size := readUint32At(f_data, int64(data_file_pos))
-		message_raw := make([]byte, message_size)
-		f_data.ReadAt(message_raw, int64(data_file_pos+4))
-		msg.Id = beside_idx_start_id
-		msg.Message = string(message_raw)
+		f_data := openFile(ctx, queue+beside_storagePart+dataFileExtension)
+		dataFilePos := uint64(0)
+		msg.Id = beside_idxStartId
+		msg.Message = readMessage(f_data, dataFilePos)
 	}
 
 	return msg
+}
+
+func readMessage(f_data *os.File, dataFilePos uint64) (message string) {
+
+	messageSize := readUint32At(f_data, int64(dataFilePos))
+	messageBytesBuff := make([]byte, messageSize)
+	f_data.ReadAt(messageBytesBuff, int64(dataFilePos+4))
+	return string(messageBytesBuff)
 }
 
 func readUint32At(f *os.File, at int64) uint32 {
@@ -318,73 +435,106 @@ func writeUint64(f *os.File, data uint64) {
 	f.Write(buff)
 }
 
-func getPartForQueue(state *State, queue string) (string, uint64) {
+func getActivePutPart(ctx *brakesContext, queue string) (int, int) {
 
-	message_id_for_new_part := uint64(1)
-	result := uint32(0)
+	queueCtx := getQueueContext(ctx, queue)
+	if !queueCtx.initialized {
 
-	f_put_state := getFile(state, queue+".pst")
+		f_putState := openFile(ctx, queue+putStateFileExtension)
+		pos, _ := f_putState.Seek(0, io.SeekEnd)
+		if pos == 0 {
+			//initialize
+			writeUint32(f_putState, uint32(queueCtx.activePartNo))
+			queueCtx.startTime = time.Now().Unix()
+			writeUint64(f_putState, uint64(queueCtx.startTime))
 
-	pos, _ := f_put_state.Seek(0, io.SeekEnd)
-	if pos == 0 {
-		//initialize
-		writeUint32(f_put_state, result)
-	} else {
-		result = readUint32At(f_put_state, 0)
-		message_id_for_new_part = readUint64At(f_put_state, 4)
+			queueCtx.firstMessageIdFowNewPart = 1
+		} else {
+			queueCtx.activePartNo = int(readUint32At(f_putState, 0))
+			queueCtx.startTime = int64(readUint64At(f_putState, 4))
+			queueCtx.firstMessageIdFowNewPart = int(readUint64At(f_putState, 4+8))
+		}
+
+		queueCtx.initialized = true
+
 	}
 
-	return strconv.Itoa(int(result)), message_id_for_new_part
+	return int(queueCtx.activePartNo), queueCtx.firstMessageIdFowNewPart
 }
 
-func switchPartForQueue(state *State, queue string, message_id_for_new_part uint64) {
+func switchPart(ctx *brakesContext, queue string, messageIdForNewPart int) (newPartNo int) {
 
-	f_put_state := getFile(state, queue+".pst")
+	queueCtx := getQueueContext(ctx, queue)
 
-	new_part := readUint32At(f_put_state, 0) + 1
-	if new_part >= 8 {
-		new_part = 0
+	f_putState := openFile(ctx, queue+putStateFileExtension)
+
+	newPartNo = int(readUint32At(f_putState, 0) + 1)
+	if newPartNo >= len(storageParts(0)) {
+		newPartNo = 0
 	}
 
-	f_idx := getFile(state, queue+"_"+strconv.Itoa(int(new_part))+".idx")
+	f_idx := openFile(ctx, queue+suffixOfPart(newPartNo)+indexFileExtension)
 	f_idx.Truncate(0)
-	f_date := getFile(state, queue+"_"+strconv.Itoa(int(new_part))+".data")
+	f_date := openFile(ctx, queue+suffixOfPart(newPartNo)+dataFileExtension)
 	f_date.Truncate(0)
 
-	f_put_state.Seek(0, io.SeekStart)
-	writeUint32(f_put_state, uint32(new_part))
-	writeUint64(f_put_state, message_id_for_new_part)
+	f_putState.Seek(0, io.SeekStart)
+	writeUint32(f_putState, uint32(newPartNo))
+	writeUint64(f_putState, uint64(time.Now().Unix()))
+	writeUint64(f_putState, uint64(messageIdForNewPart))
+
+	queueCtx.activePartNo = newPartNo
+	queueCtx.firstMessageIdFowNewPart = messageIdForNewPart
+
+	return newPartNo
 }
 
-func getFile(state *State, name string) *os.File {
+func openFile(ctx *brakesContext, name string) *os.File {
 
-	res := state.files[name]
+	res := ctx.files[name]
 
 	if res == nil {
 		var err error
-		res, err = os.OpenFile(`d:\temp\`+name, os.O_CREATE|os.O_RDWR, 0644)
+		res, err = os.OpenFile(ctx.rootDataDir+name, os.O_CREATE|os.O_RDWR, 0644)
 		if err != nil {
 			println(err)
 		}
-		state.files[name] = res
+		ctx.files[name] = res
 	}
 
 	return res
 
 }
 
-func Put(state *State, queue string, message string) {
+func messageCountByIndexFileSize(indexFileSize int) int {
+	return int(indexFileSize>>3) - 1
+}
 
-	part, message_id_for_new_part := getPartForQueue(state, queue)
+func Put(ctx *brakesContext, queue string, message string) {
 
-	f_idx := getFile(state, queue+"_"+part+".idx")
+	/*
+		var b bytes.Buffer
+		w := zlib.NewWriter(&b)
+		w.Write([]byte(message))
+		w.Close()
+		f_data.Write(b.Bytes())
+	*/
 
-	pos, _ := f_idx.Seek(0, io.SeekEnd)
-	if pos == 0 {
-		writeUint64(f_idx, message_id_for_new_part)
+	queueCtx := getQueueContext(ctx, queue)
+	queueCtx.Lock()
+
+	defer queueCtx.Unlock()
+
+	storagePart, firstMessageIdFowNewPart := getActivePutPart(ctx, queue)
+
+	f_idx := openFile(ctx, queue+suffixOfPart(storagePart)+indexFileExtension)
+
+	indexFileSize, _ := f_idx.Seek(0, io.SeekEnd)
+	if indexFileSize == 0 {
+		writeUint64(f_idx, uint64(firstMessageIdFowNewPart))
 	}
 
-	f_data := getFile(state, queue+"_"+part+".data")
+	f_data := openFile(ctx, queue+suffixOfPart(storagePart)+dataFileExtension)
 
 	offset_data, _ := f_data.Seek(0, io.SeekEnd)
 
@@ -393,10 +543,18 @@ func Put(state *State, queue string, message string) {
 
 	writeUint64(f_idx, uint64(offset_data))
 
-	if offset_data >= 1000000000 {
-		current_start_message_id := readUint64At(f_idx, 0)
-		pos, _ := f_idx.Seek(0, io.SeekEnd)
-		switchPartForQueue(state, queue, current_start_message_id+uint64(pos)>>3-1)
+	updatePredictData(ctx, queue, storagePart,
+		int(firstMessageIdFowNewPart),
+		messageCountByIndexFileSize(int(indexFileSize))+1)
+
+	daysElapsed := (time.Now().Unix() - getQueueContext(ctx, queue).startTime) / 60 / 60 / 24
+	if offset_data >= 100000000 && daysElapsed > 30 {
+		currentStartMessageId := int(readUint64At(f_idx, 0))
+		idx_fileLen, _ := f_idx.Seek(0, io.SeekEnd)
+		firstMessageId := currentStartMessageId + messageCountByIndexFileSize(int(idx_fileLen))
+		newPartNo := switchPart(ctx, queue, firstMessageId)
+
+		updatePredictData(ctx, queue, newPartNo, firstMessageId, 0)
 	}
 
 }
